d1c729329143f4e3c21dcd7bc39e26246ad6461b
Arafat0122 <soyebarafat0122@yahoo.com>
2024-07-03 16:49:42 +0600

    foorter added

---
+++ src/components/Logo2.jsx
@@ -0,0 +1,14 @@
+
+
+const Logo2 = () => {
+    return (
+        <div>
+            <div className="flex flex-col items-center gap-2">
+                <img className="w-12" src="https://i.ibb.co/LpDc4S3/arafat-icon.png" alt="" />
+                <p className="font-tourney font-extrabold text-2xl hidden lg:flex">So<span className="text-red-600">y</span>eb A<span className="text-red-600">h</span>med <span className="text-red-600">A</span>rafat</p>
+            </div>
+        </div>
+    );
+};
+
+export default Logo2;
